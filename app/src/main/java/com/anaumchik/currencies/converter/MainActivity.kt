17db2566983b9b30bc0adad1aaac8b5d271f4640
anaumchik <aliaksandr.naumchik@gmail.com>
2020-06-03 12:56:41 +0300

    initial commit

---
+++ app/src/main/java/com/anaumchik/currencies/converter/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.anaumchik.currencies.converter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anaumchik.currencies.converter.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
