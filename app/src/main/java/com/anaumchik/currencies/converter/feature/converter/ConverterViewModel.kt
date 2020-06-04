b288453eb4dc1f2ba843d0e7ffbb139774b99a62
anaumchik <aliaksandr.naumchik@gmail.com>
2020-06-04 18:18:17 +0300

    refresh every second

--- app/src/main/java/com/anaumchik/currencies/converter/feature/converter/ConverterViewModel.kt
+++ app/src/main/java/com/anaumchik/currencies/converter/feature/converter/ConverterViewModel.kt
@@ -8,7 +8,9 @@ import com.anaumchik.currencies.converter.feature.converter.interactor.Converter
 import com.anaumchik.currencies.converter.models.Currency
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ConverterViewModel(private val converterInteractor: ConverterInteractor) : ViewModel() {
 
@@ -16,11 +18,13 @@ class ConverterViewModel(private val converterInteractor: ConverterInteractor) :
     val currenciesLiveData: LiveData<List<Currency>> = _currenciesLiveData
 
     fun onStartUpdates() {
-        viewModelScope.launch(Dispatchers.IO) {
-//            while (true) {
-            getCurrencies()
-//                delay(1000)
-//            }
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                while (true) {
+                    getCurrencies()
+                    delay(2000)
+                }
+            }
         }
     }
 
@@ -46,6 +50,7 @@ class ConverterViewModel(private val converterInteractor: ConverterInteractor) :
         val baseTotal = baseCurrency.currencyRate * baseCurrency.currencyTotal
 
         val updatedCurrencies = updateTotal(currencies, baseTotal)
+
         _currenciesLiveData.postValue(updatedCurrencies)
     }
 
